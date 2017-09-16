d890e0bca7d0609c61000713e6ac69fb3a6d6ff3
Roshita Shakya <roshi.shk@gmail.com>
2017-09-16 09:01:38 +0000

    Creating posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :photo, PhotoUploader
+  has_many :posts
 end
