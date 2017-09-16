3a6e7b11c3736a785b19017e28bab9ee043c38c2
Roshita Shakya <roshi.shk@gmail.com>
2017-09-16 11:09:38 +0000

    Delete comments successful

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!, only: :create
+    before_action :is_owner?, only: :destroy
+
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
@@ -7,6 +9,19 @@ class CommentsController < ApplicationController
            redirect_to root_path
         else
             flash[:alert] = "Invalid attributes."
+            redirect_to root_path
+        end
+    end
+    
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to root_path
+    end
+    
+    def is_owner?
+        @comment = Comment.find(params[:id])
+        if @comment.user != current_user
             redirect_to root_path
         end
     end
