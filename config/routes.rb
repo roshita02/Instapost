d5cb0076f6e0e8be24071870dbebb2a5f795d6c9
Roshita Shakya <roshi.shk@gmail.com>
2017-09-18 18:19:26 +0000

    Setting up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts do
-    resources :comments
+  resources :comments
+  resources :upvotes, only: :create
+  resources :downvotes, only: :create
   end
   resources :users, only: :show
 end
---
+++ test/controllers/downvotes_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+class DownvotesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post.downvote_from current_user
+    redirect_to posts_path
+  end
+end
