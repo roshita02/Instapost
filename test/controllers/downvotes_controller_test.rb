4b7bfc2bc9fa802ea028841291e8626e58d95f60
Roshita Shakya <roshi.shk@gmail.com>
2017-09-21 10:55:08 +0000

    Sending AJAX request+ error fixed

--- test/controllers/downvotes_controller_test.rb
+++ test/controllers/downvotes_controller_test.rb
@@ -1,10 +1,7 @@
 require 'test_helper'
-class DownvotesController < ApplicationController
-  before_action :authenticate_user!
-
-  def create
-    @post = Post.find(params[:post_id])
-    @post.downvote_from current_user
-    redirect_to posts_path
-  end
+ 
+class DownvotesControllerTest < ActionDispatch::IntegrationTest
+# test "the truth" do
+#   assert true
+# end
 end
--- test/controllers/upvotes_controller_test.rb
+++ test/controllers/upvotes_controller_test.rb
@@ -1,10 +1,7 @@
 require 'test_helper'
 
-class UpvotesController < ApplicationController
-  before_action :authenticate_user!
-  def create
-  @post = Post.find(params[:post_id])
-  @post.liked_by current_user
-  redirect_to posts_path
-  end
+class UpvotesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
 end
